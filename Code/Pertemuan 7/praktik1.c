c7301dbdd1f879776267a73287706d27ecc764aa
Rasyid Firmansyah <rasyidfirmansyah56@student.uns.ac.id>
2024-11-12 14:20:18 +0700

    rename function

--- Code/Pertemuan 7/praktik1.c
+++ Code/Pertemuan 7/praktik1.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
-void rekrusi(int i){
+void rekursi(int i)
+{
     printf("Hello World!\n");
-    if(i == 1){
+    if (i == 1)
+    {
         return;
     }
-    return rekrusi(--i);
+
+    return rekursi(--i);
+
+    // rekursi(i--);
+    // printf("kelewatan\n");
 }
 
 int main(int argc, char const *argv[])
 {
-    rekrusi(5);
+    rekursi(5);
     return 0;
 }
